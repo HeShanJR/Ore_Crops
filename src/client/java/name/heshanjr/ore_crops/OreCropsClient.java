384ee708dd9cde65b2b96987f775fc416712e157
HeShanJR <1693875793@qq.com>
2025-08-25 09:03:31 +0800

    Initial commit初始提交

---
+++ src/client/java/name/heshanjr/ore_crops/OreCropsClient.java
@@ -0,0 +1,10 @@
+package name.heshanjr.ore_crops;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OreCropsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
